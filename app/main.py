818c47deeff864f612e9108c142e2aadffedec93
Raphael Bassot <rbassot@shaw.ca>
2020-02-08 11:43:48 -0800

    Init testing

--- app/main.py
+++ app/main.py
@@ -62,7 +62,7 @@ def move():
     print(json.dumps(data))
 
     directions = ['up', 'down', 'left', 'right']
-    direction = random.choice(directions)
+    direction = 'up'
 
     return move_response(direction)
 
